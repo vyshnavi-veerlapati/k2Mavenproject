ae1bb38e1442a97078d4620de2505f615771e5af
vyshnavi <vyshnaviveerlapati@gmail.com>
2024-11-15 23:32:50 +0530

    commit 1

---
+++ src/main/java/com/app/demo/hospitalmanagementsystem/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.hospitalmanagementsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Hospital Management System!" );
+    }
+}
