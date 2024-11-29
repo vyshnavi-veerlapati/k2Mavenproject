5a8d00587731d264c81266fbb5d2697b9fb0b8a3
vyshnavi-veerlapati <147941270+vyshnavi-veerlapati@users.noreply.github.com>
2024-11-29 10:10:44 +0530

    Update App.java

--- src/main/java/com/app/demo/hospitalmanagementsystem/App.java
+++ src/main/java/com/app/demo/hospitalmanagementsystem/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Hospital Management System!" );
+        System.out.println( "Hello Hospital Management System! 22BD1A66K2" );
     }
 }
